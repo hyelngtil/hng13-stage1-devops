19a2ac619bc619289c40339379d556755a868f58
hyelngtil <isaachyelngtil@gmail.com>
2025-10-22 01:57:22 +0100

    Modify Fapp.py view

--- Fapp.py
+++ Fapp.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def home():
     return f'''
     <html>
-        <body style="font-family: Arial; text-align: center; padding: 50px;">
-            <h1>🚀 Deployment Successful!</h1>
+        <body style="font-family: Arial; text-align: center; padding: 50px; background-color: #2a629a;">
+            <h1>🚀Stage1 HNG13 Deployment Successful!</h1>
             <p>Your automated deployment script is working!</p>
             <p>Server Time: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}</p>
         </body>
